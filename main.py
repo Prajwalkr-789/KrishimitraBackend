556f07113f9d4911aad123a209cd2765a99b7375
Prajwalkr-789 <prajwalkrpkrp@gmail.com>
2025-08-18 20:48:15 +0530

    Modified main.py

--- main.py
+++ main.py
@@ -213,4 +213,4 @@ def text_query():
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))
-    app.run(host="0.0.0.0",port = port,debug=True)
+    app.run(host="0.0.0.0",port = port)
